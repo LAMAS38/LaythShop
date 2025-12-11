f2c90164dd71cc09372fa9301f6f898d12a48c31
LAMAS38 <LAMAS38@ulaval.ca>
2025-12-11 18:50:42 -0500

    feat: initialisation du projet LaythShop avec Next.js, ESLint, App Router, TypeScript et Tailwind

---
+++ laythshop/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <h1 className="text-4xl font-bold text-center">
+        Bienvenue sur LaythShop 🛍️
+      </h1>
+      <p className="text-center mt-4 text-gray-600">
+        Ta boutique e-commerce en construction !
+      </p>
+    </main>
+  );
+}
